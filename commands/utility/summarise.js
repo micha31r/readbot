d2b04867301c3d1c604394e124c366f1cf23cd38
Michael Ren <liyur@student.unimelb.edu.au>
2025-04-23 05:40:05 +1000

    Bug fix

--- commands/utility/summarise.js
+++ commands/utility/summarise.js
@@ -64,7 +64,7 @@ module.exports = {
 					{ name: '100', value: 100 },
 					{ name: '200', value: 200 },
 					{ name: '500', value: 500 },
-					{ name: '1000', value: 1000 }
+					{ name: '1000', value: 1000 },
 					{ name: '2000', value: 2000 }
 				)
 		)
